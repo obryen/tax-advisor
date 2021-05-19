bb5ec1ceb4a27e9c87c4c9198bbeb8210c030a03
Peter Gundel <mail@petergundel.de>
2021-05-19 08:23:59 +0200

    Use paths in tsconfig

--- src/app.resolver.spec.ts
+++ src/app.resolver.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppResolver } from './app.resolver';
+import { AppResolver } from '@src/app.resolver';
 
 describe(AppResolver.name, () => {
   let appResolver: AppResolver;
